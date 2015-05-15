dea0313f4b28f6bf2e9e61a2f49c957eaea2554e
Bryan Bishop <bryan.bishop@pearson.com>
2015-05-15 09:54:43 -0500

    simplified the ownership to just a me variable

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,6 @@
-unless ::File.exist?("#{::Dir.home(node['current_user'])}/Google\ Drive/.placeholder.txt")
+me = node['current_user']
+
+unless ::File.exist?("#{::Dir.home(me)}/Google\ Drive/.placeholder.txt")
   Chef::Log.info "\n\n\n\n\t\tPlease execute the Google Drive sync app and rerun kitchenplan.\n\n\n\n"
   return
 end
@@ -7,15 +9,15 @@ Chef::Log.info "\n\n\n\Google Drive appears to be enabled.\n\n\n"
 
 directory "Applications" do
   mode "0700"
-  owner node['current_user']
-  group node['current_user']
+  owner me
+  group me
 end
 
 directory ".keepassx" do
-  path ::File.join(::Dir.home(node['current_user']), ".keepassx")
+  path ::File.join(::Dir.home(me), ".keepassx")
   mode "0700"
-  owner node['current_user']
-  group node['current_user']
+  owner me
+  group me
 end
 
 %w(
@@ -24,11 +26,11 @@ personal-config.ini
 work-config.ini
 ).each do |config|
   template config do
-    path ::File.join(::Dir.home(node['current_user']), ".keepassx", config)
+    path ::File.join(::Dir.home(me), ".keepassx", config)
     source "#{config}.erb"
     mode "0644"
-    owner node['current_user']
-    group node['current_user']
+    owner me
+    group me
   end
 end
 
@@ -37,16 +39,16 @@ KeePassX.app
 mykeepassess.app
 ).each do |app|
   link app do
-    target_file ::File.join(::Dir.home(node['current_user']), "Applications", app)
-    to ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", app)
+    target_file ::File.join(::Dir.home(me), "Applications", app)
+    to ::File.join(::Dir.home(me), "Google Drive", "keepassx", app)
   end
 end
 
-file ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", "KeePassX.app", "Contents", "MacOS", "KeePassX") do
+file ::File.join(::Dir.home(me), "Google Drive", "keepassx", "KeePassX.app", "Contents", "MacOS", "KeePassX") do
   action :touch
   mode "0755"
-  owner node['current_user']
-  group node['current_user']
+  owner me
+  group me
 end
 
 %w(
@@ -54,15 +56,15 @@ personal.keyfile
 work.keyfile
 ).each do |key|
   link "secret key files: #{key}" do
-    target_file ::File.join(::Dir.home(node['current_user']), key)
-    to ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", key)
+    target_file ::File.join(::Dir.home(me), key)
+    to ::File.join(::Dir.home(me), "Google Drive", "keepassx", key)
   end
 end
 
 file "mykeepasses.sh" do
   action :touch
-  path ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", "mykeepasses.sh")
+  path ::File.join(::Dir.home(me), "Google Drive", "keepassx", "mykeepasses.sh")
   mode "0755"
-  owner node['current_user']
-  group node['current_user']
+  owner me
+  group me
 end
