d0e579ad510f455b4dc1a80ebd71ecaa4148ae1e
Bryan Bishop <bryan.bishop@pearson.com>
2015-05-14 23:46:39 -0500

    reduce complexity a bit

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,17 +8,14 @@ Chef::Log.info "\n\n\n\Google Drive appears to be enabled.\n\n\n"
 default_owner = {  :owner => node['current_user'], :group => node['current_user'] }
 
 directory "Applications" do
-  action :create
   path ::File.join(::Dir.home(node['current_user']), "Applications")
   default_owner
   mode "0700"
 end
 
 directory ".keepassx" do
-  action :create
   path ::File.join(::Dir.home(node['current_user']), ".keepassx")
-  owner node['current_user']
-  group node['current_user']
+  default_owner
   mode "0700"
 end
 
@@ -28,12 +25,10 @@ personal-config.ini
 work-config.ini
 ).each do |config|
   template config do
-    action :create
     path ::File.join(::Dir.home(node['current_user']), ".keepassx", config)
     source "#{config}.erb"
     mode "0644"
-    owner node['current_user']
-    group node['current_user']
+    default_owner
   end
 end
 
@@ -42,7 +37,6 @@ KeePassX.app
 mykeepassess.app
 ).each do |app|
   link app do
-    action :create
     target_file ::File.join(::Dir.home(node['current_user']), "Applications", app)
     to ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", app)
   end
@@ -50,8 +44,7 @@ end
 
 file ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", "KeePassX.app", "Contents", "MacOS", "KeePassX") do
   action :touch
-  owner node['current_user']
-  group node['current_user']
+  default_owner
   mode "0755"
 end
 
@@ -60,7 +53,6 @@ personal.keyfile
 work.keyfile
 ).each do |key|
   link "secret key files: #{key}" do
-    action :create
     target_file ::File.join(::Dir.home(node['current_user']), key)
     to ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", key)
   end
@@ -69,7 +61,6 @@ end
 file "mykeepasses.sh" do
   action :touch
   path ::File.join(::Dir.home(node['current_user']), "Google Drive", "keepassx", "mykeepasses.sh")
-  owner node['current_user']
-  group node['current_user']
+  default_owner
   mode "0755"
 end
