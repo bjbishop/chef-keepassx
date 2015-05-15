51d741869fee771c3861d25cbcda895dfe2e552b
Bryan Bishop <bryan.bishop@pearson.com>
2015-05-14 23:41:21 -0500

    trying out reusing owner and group

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,17 @@
 unless ::File.exist?("#{::Dir.home(node['current_user'])}/Google\ Drive/.placeholder.txt")
-  Chef::Log.info "\n\n\n\n\t\tYou can now run Dropbox and Google Drive and rerun kitchenplan.\n\n\n\n"
+  Chef::Log.info "\n\n\n\n\t\tPlease execute the Google Drive sync app and rerun kitchenplan.\n\n\n\n"
   return
 end
 
 Chef::Log.info "\n\n\n\Google Drive appears to be enabled.\n\n\n"
 
+default_owner = {  :owner => node['current_user'], :group => node['current_user'] }
+
 directory "Applications" do
   action :create
   path ::File.join(::Dir.home(node['current_user']), "Applications")
-  owner node['current_user']
-  group node['current_user']
+  default_owner
   mode "0700"
-  recursive false
 end
 
 directory ".keepassx" do
@@ -20,7 +20,6 @@ directory ".keepassx" do
   owner node['current_user']
   group node['current_user']
   mode "0700"
-  recursive false
 end
 
 %w(
